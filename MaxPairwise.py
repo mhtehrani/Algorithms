d61ede4e30759927e6aa4c92d0062a9afc9b7e28
Mohammad H. Tehrani <tehran_moonlight@yahoo.com>
2021-09-22 22:20:46 -0700

    Add files via upload
    Maximum Pairwise Product Problem
    
    Find the maximum product of two distinct numbers in a sequence of non-negative integers.
    
    Input: A sequence of non-negative integers.
    Output: The maximum value that can be obtained by multiplying two different elements from the sequence.
    
    Input format. The first line contains an integer n. The next line contains n non-negative integers a1, ..., an (separated by spaces).
    Output format. The maximum pairwise product.
    Constraints. 2 < n < 2 *10^5; 0 < a1, ..., an < 2*10^5.

---
+++ MaxPairwise.py
@@ -0,0 +1,23 @@
+"""
+Maximum Pairwise Product
+========================
+
+@author: mhtehrani
+May 4, 2021
+https://github.com/mhtehrani
+
+"""
+
+
+def max_pairwise_product(numbers):
+    i1 = numbers.index(max(numbers))
+    max1 = numbers[i1]
+    temp = numbers[0:i1]+numbers[i1+1:]
+    i2 = temp.index(max(temp))
+    max2 = temp[i2]
+    return max1*max2
+
+if __name__ == '__main__':
+    input_n = int(input())
+    input_numbers = [int(x) for x in input().split()]
+    print(max_pairwise_product(input_numbers))
