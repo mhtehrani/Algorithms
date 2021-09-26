ebfa9a2d6eae50d978dfd94f4b85045452267fd2
Mohammad H. Tehrani <tehran_moonlight@yahoo.com>
2021-09-26 13:12:02 -0700

    Add files via upload
    Last Digit of the Sum of Fibonacci Numbers
    ==========================================
    Given two non-negative integers 𝑚 and 𝑛, where 𝑚 ≤ 𝑛, find the last digit of the sum 𝐹𝑚 + 𝐹𝑚+1 + · · · + 𝐹𝑛.
    Input Format: The input consists of two non-negative integers 𝑚 and 𝑛 separated by a space.
    Constraints: 0 ≤ 𝑚 ≤ 𝑛 ≤ 1014.
    Output Format: Output the last digit of 𝐹𝑚 + 𝐹𝑚+1 + · · · + 𝐹𝑛.

---
+++ fibonacci_partial_sum.py
@@ -0,0 +1,70 @@
+"""
+Last Digit of a Large Fibonacci Number
+======================================
+
+@author: mhtehrani
+July 2, 2021
+https://github.com/mhtehrani
+
+"""
+# Uses python3
+
+def fibonacci_sum_fast(n):
+    period = 60
+    
+    def sum_n(n):
+        if n <= 1:
+            return n
+    
+        previous = 0
+        current  = 1
+        sum      = 1
+
+        for _ in range(n - 1):
+            previous, current = current, previous + current
+            sum += current
+        return sum
+    
+    if n <= 1:
+        return n
+    
+    if n <= period:
+        return sum_n(n) % 10
+    
+    if n > period:
+        return int(n/period)*(sum_n(period) % 10) + (sum_n(n-period*int(n/period)) % 10)
+
+
+
+def fibonacci_partial_sum_naive(from_, to):
+    sum = 0
+
+    current = 0
+    next  = 1
+
+    for i in range(to + 1):
+        if i >= from_:
+            sum += current
+
+        current, next = next, current + next
+
+    return sum % 10
+
+
+
+def fibonacci_partial_sum_fast(from_, to):
+    if from_ == 0:
+        sum_from = 0
+    else:
+        sum_from = fibonacci_sum_fast(from_-1)
+    
+    sum_to = fibonacci_sum_fast(to)
+    
+    if sum_to >= sum_from:
+        return sum_to - sum_from
+    else:
+        return (sum_to + 10) - sum_from
+
+from_, to = map(int, input().split())
+# print(fibonacci_partial_sum_naive(from_, to))
+print(fibonacci_partial_sum_fast(from_, to))
