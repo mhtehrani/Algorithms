aa8ccd7646668034e8de4a0750cd56a9b0d13ac1
Mohammad H. Tehrani <tehran_moonlight@yahoo.com>
2021-09-26 13:08:43 -0700

    Update fibonacci_sum_last_digit.py
    Last Digit of the Sum of Fibonacci Numbers
    ==========================================
    Given an integer 𝑛, find the last digit of the sum 𝐹0 + 𝐹1 + · · · + 𝐹𝑛.
    Input Format: The input consists of a single integer 𝑛.
    Constraints: 0 ≤ 𝑛 ≤ 1014.
    Output Format: Output the last digit of 𝐹0 + 𝐹1 + · · · + 𝐹𝑛.

--- fibonacci_sum_last_digit.py
+++ fibonacci_sum_last_digit.py
@@ -7,23 +7,6 @@ July 2, 2021
 https://github.com/mhtehrani
 
 """
-# Uses python3
-# import timeit
-
-def fibonacci_sum_naive(n):
-    if n <= 1:
-        return n
-
-    previous = 0
-    current  = 1
-    sum      = 1
-
-    for _ in range(n - 1):
-        previous, current = current, previous + current
-        sum += current
-
-    return sum % 10
-
 
 def fibonacci_sum_fast(n):
     period = 60
@@ -53,12 +36,4 @@ def fibonacci_sum_fast(n):
 
 n = int(input())
 
-# a1 = timeit.default_timer()
-# print(fibonacci_sum_naive(n))
-# b1 = timeit.default_timer()
 print(fibonacci_sum_fast(n))
-# c1 = timeit.default_timer()
-
-# print(b1-a1)
-# print(c1-b1)
-
