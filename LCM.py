132b4994e4f949f549b8673c98eb14a8e9d44ba7
Mohammad H. Tehrani <tehran_moonlight@yahoo.com>
2021-09-26 12:59:25 -0700

    Update LCM.py
    Least Common Multiple
    =====================
    Given two integers 𝑎 and 𝑏, find their least common multiple.
    Input Format: The two integers 𝑎 and 𝑏 are given in the same line separated by space.
    Constraints: 1 ≤ 𝑎, 𝑏 ≤ 107.
    Output Format: Output the least common multiple of 𝑎 and 𝑏.

--- LCM.py
+++ LCM.py
@@ -7,15 +7,6 @@ July 1, 2021
 https://github.com/mhtehrani
 
 """
-# Uses python3
-# import timeit
-
-def lcm_naive(a, b):
-    for l in range(1, a*b + 1):
-        if l % a == 0 and l % b == 0:
-            return l
-
-    return a*b
 
 def gcd_fast(a, b):
     while True:
@@ -33,11 +24,4 @@ def lcm_fast(a, b):
 
 a, b = map(int, input().split())
 
-# a1 = timeit.default_timer()
-# print(lcm_naive(a, b))
-# b1 = timeit.default_timer()
 print(lcm_fast(a, b))
-# c1 = timeit.default_timer()
-
-# print(b1-a1)
-# print(c1-b1)
