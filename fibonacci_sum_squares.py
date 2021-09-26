000ad198ad0a2de87205563d65916f5bcedb8bb6
Mohammad H. Tehrani <tehran_moonlight@yahoo.com>
2021-09-26 13:15:47 -0700

    Update fibonacci_sum_squares.py
    Last Digit of the Sum of Squares of Fibonacci Numbers
    =====================================================
    Compute the last digit of 𝐹0^2 + 𝐹1^2 + · · · + 𝐹𝑛^2.
    Input Format: Integer 𝑛.
    Constraints: 0 ≤ 𝑛 ≤ 1014.
    Output Format: The last digit of 𝐹0^2 + 𝐹1^2 + · · · + 𝐹𝑛^2.

--- fibonacci_sum_squares.py
+++ fibonacci_sum_squares.py
@@ -7,22 +7,6 @@ July 2, 2021
 https://github.com/mhtehrani
 
 """
-# Uses python3
-
-def fibonacci_sum_squares_naive(n):
-    if n <= 1:
-        return n
-
-    previous = 0
-    current  = 1
-    sum      = 1
-
-    for _ in range(n - 1):
-        previous, current = current, previous + current
-        sum += current * current
-
-    return sum % 10
-
 
 def fibonacci_sum_squares_fast(n):
     period = 60
@@ -52,5 +36,5 @@ def fibonacci_sum_squares_fast(n):
 
 
 n = int(input())
-# print(fibonacci_sum_squares_naive(n))
+
 print(fibonacci_sum_squares_fast(n))
