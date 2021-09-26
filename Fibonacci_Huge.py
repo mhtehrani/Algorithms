132868a4a1cef94048d82807e15e24bfc29983b6
Mohammad H. Tehrani <tehran_moonlight@yahoo.com>
2021-09-26 13:04:37 -0700

    Update Fibonacci_Huge.py
    Fibonacci Number mode 𝑚
    ========================
    Given two integers 𝑛 and 𝑚, output 𝐹𝑛 mod 𝑚 (that is, the remainder of 𝐹𝑛 when divided by 𝑚).
    Input Format: The input consists of two integers 𝑛 and 𝑚 given on the same line (separated by a space).
    Constraints: 1 ≤ 𝑛 ≤ 1014, 2 ≤ 𝑚 ≤ 103.
    Output Format: Output 𝐹𝑛 mod 𝑚.

--- Fibonacci_Huge.py
+++ Fibonacci_Huge.py
@@ -7,8 +7,6 @@ July 2, 2021
 https://github.com/mhtehrani
 
 """
-# Uses python3
-import timeit
 
 def pisanoPeriod(m):
     previous, current = 0, 1
@@ -20,7 +18,6 @@ def pisanoPeriod(m):
         if (previous == 0 and current == 1):
             return i + 1
 
-
 def calc_fib_fast(n):
     f = [0]
     f.append(1)
@@ -31,34 +28,11 @@ def calc_fib_fast(n):
             f.append(f[i-1]+f[i-2])
         return f[n]
 
-# def get_fibonacci_huge_naive(n, m):
-    if n <= 1:
-        return n
-
-    previous = 0
-    current  = 1
-
-    for _ in range(n - 1):
-        previous, current = current, previous + current
-    # print(current)
-    return current % m
-
-
 def get_fibonacci_huge_fast(n, m):
     mode_length = pisanoPeriod(m)
     n_new = n % mode_length
     return calc_fib_fast(n_new) % m
 
-
-
-
 n, m = map(int, input().split())
 
-# a1 = timeit.default_timer()
-# print(get_fibonacci_huge_naive(n, m))
-# b1 = timeit.default_timer()
 print(get_fibonacci_huge_fast(n, m))
-# c1 = timeit.default_timer()
-
-# print(b1-a1)
-# print(c1-b1)
