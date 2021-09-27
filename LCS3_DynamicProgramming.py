7eba7d1b4aefdb12ac163499cdf48c005935a5b0
Mohammad H. Tehrani <tehran_moonlight@yahoo.com>
2021-09-26 20:02:58 -0700

    Update LCS3_DynamicProgramming.py
    Longest Common Subsequence of Three Sequences
    =============================================
    Given three sequences 𝐴 = (𝑎1, 𝑎2, . . . , 𝑎𝑛), 𝐵 = (𝑏1, 𝑏2, . . . , 𝑏𝑚), and 𝐶 = (𝑐1, 𝑐2, . . . , 𝑐𝑙), finding the length of their longest common subsequence, i.e., the largest non-negative integer 𝑝 such that there exist indices 1 ≤ 𝑖1 < 𝑖2 < · · · < 𝑖𝑝 ≤ 𝑛, 1 ≤ 𝑗1 < 𝑗2 < · · · < 𝑗𝑝 ≤ 𝑚, 1 ≤ 𝑘1 < 𝑘2 < · · · < 𝑘𝑝 ≤ 𝑙 such that 𝑎𝑖1 = 𝑏𝑗1 = 𝑐𝑘1 , . . . , 𝑎𝑖𝑝 = 𝑏𝑗𝑝 = 𝑐𝑘𝑝
    Input Format: First line: 𝑛. Second line: 𝑎1, 𝑎2, . . . , 𝑎𝑛. Third line: 𝑚. Fourth line: 𝑏1, 𝑏2, . . . , 𝑏𝑚. Fifth line: 𝑙. Sixth line: 𝑐1, 𝑐2, . . . , 𝑐𝑙.
    Constraints: 1 ≤ 𝑛, 𝑚, 𝑙 ≤ 100; −10^9 < 𝑎𝑖, 𝑏𝑖, 𝑐𝑖 < 10^9.
    Output Format: Output 𝑝.

--- LCS3_DynamicProgramming.py
+++ LCS3_DynamicProgramming.py
@@ -27,7 +27,7 @@ def LCS3_fast(row, col, dep):
     #end
     
     # Creating empty table of LCSs and initializing the 1st row and column with 0
-    # =========================================================================
+    # ===========================================================================
     LCSs = [None]*(len(row)+1)
     for i in range(len(row)+1):
         LCSs[i] = [None]*(len(col)+1)
@@ -37,48 +37,23 @@ def LCS3_fast(row, col, dep):
     #end
     
     # Filling the Sequence and LCS tables
-    # =========================================================================
+    # ===================================
     for i in range(1,len(row)+1):
         for j in range(1,len(col)+1):
             for k in range(1,len(dep)+1):
-                # print(row[i-1] , col[j-1], dep[k-1])
-                
-                # Seq[i][j][k] = Seq[i-1][j][k].copy()
-                # for l in Seq[i][j-1][k]:
-                #     Seq[i][j][k].append(l.copy())
-                # #end
-                # for l in Seq[i][j][k-1]:
-                #     Seq[i][j][k].append(l.copy())
-                # #end
-                
                 # if they match
                 if row[i-1] == col[j-1]== dep[k-1]:
                     LCSs[i][j][k] = LCSs[i-1][j-1][k-1] +1
-                    # if len(Seq[i-1][j-1][k-1]) == 0:
-                    #     Seq[i][j][k].append([row[i-1]])
-                    # else:
-                    #     for l in range(len(Seq[i-1][j-1][k-1])):
-                    #         temp = Seq[i-1][j-1][k-1][l].copy()
-                    #         temp.append(row[i-1])
-                    #         Seq[i][j][k].append(temp)
-                    #     #end
-                    # #end
                 
                 # if they don't match
                 else:
                     LCSs[i][j][k] = max(LCSs[i-1][j][k], LCSs[i][j-1][k], LCSs[i][j][k-1])
                 #end
-            
-                # print(Seq)
             #end
         #end
     #end
     
-    return LCSs[len(row)][len(col)][len(dep)]#, Seq[len(row)][len(col)][len(dep)]
-
-
-
-
+    return LCSs[len(row)][len(col)][len(dep)]
 
 
 n = int(input())
@@ -91,34 +66,4 @@ col = list(map(int, input().split()))
 l = int(input())
 dep = list(map(int, input().split()))
 
-# n = 3
-# row = [1,2,3]
-# m = 3
-# col = [2,1,3]
-# l = 3
-# dep = [1,3,5]
-
 print(LCS3_fast(row, col, dep))
-
-# ROW = []
-# LCS3 = 0
-# Temp = SEQ(row, col)
-# for i in Temp:
-#     if i not in ROW:
-#         ROW.append(i)
-#     #end
-# #end
-# for i in ROW:
-#     j = LCS_fast(i, COL)
-#     if j >= LCS3:
-#         LCS3 = j
-#     #end
-# #end
-
-# b1 = LCS_fast(a1, a2)
-# b2 = LCS_fast(a1, a3)
-# b3 = LCS_fast(a2, a3)
-
-# print(min(b1,b2,b3))
-
-
