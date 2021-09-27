ca5516da1b594e04703e660c4db7ffd66e5ebaea
Mohammad H. Tehrani <tehran_moonlight@yahoo.com>
2021-09-26 17:14:32 -0700

    Update DifferentSummands.py
    Maximum Number of Prizes
    ========================
    The goal is to represent a given positive integer 𝑛 as a sum of as many pairwise distinct positive integers as possible. That is, to find the maximum 𝑘 such that 𝑛 can be written as 𝑎1 + 𝑎2 + · · · + 𝑎𝑘 where 𝑎1, . . . , 𝑎𝑘 are positive integers and 𝑎𝑖 ̸= 𝑎𝑗 for all 1 ≤ 𝑖 < 𝑗 ≤ 𝑘.
    Input Format: The input consists of a single integer 𝑛.
    Constraints: 1 ≤ 𝑛 ≤ 109.
    Output Format: In the first line, output the maximum number 𝑘 such that 𝑛 can be represented as a sum of 𝑘 pairwise distinct positive integers. In the second line, output 𝑘 pairwise distinct positive integers that sum up to 𝑛.

--- DifferentSummands.py
+++ DifferentSummands.py
@@ -8,35 +8,6 @@ https://github.com/mhtehrani
 
 """
 
-def optimal_summands(n):
-    summands = []
-    
-    check = True
-        
-    while check:
-        if n > len(summands):
-            for i in range(len(summands)):
-                summands[i] += 1
-                n -= 1
-            summands.append(1)
-            n -= 1
-            
-        
-        elif n == len(summands):
-            for i in range(len(summands)):
-                summands[i] += 1
-                n -= 1
-        else:
-            summands[0] += n
-            n -= n
-        
-        if n==0:
-            check = False
-            
-    summands.sort()
-    return summands
-
-
 def optimal_summands_fast(n):
     k = int((-1+(1+4*2*n)**0.5)/2)
     summands = []
@@ -49,12 +20,6 @@ def optimal_summands_fast(n):
 
 
 n = int(input())
-
-# summands = optimal_summands(n)
-# print(len(summands))
-# for x in summands:
-#     print(x, end=' ')
-
 
 summands_final = optimal_summands_fast(n)
 print(len(summands_final))
