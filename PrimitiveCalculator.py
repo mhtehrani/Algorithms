8ee41d7928bbefe80fca315c6ed644ec558fb2cc
Mohammad H. Tehrani <tehran_moonlight@yahoo.com>
2021-09-26 19:44:39 -0700

    Add files via upload
    Primitive Calculator
    ====================
    Given an integer 𝑛, compute the minimum number of operations needed to obtain the number 𝑛 starting from the number 1 by performing the following three operations with the current number 𝑥: multiply 𝑥 by 2, multiply 𝑥 by 3, or add 1 to 𝑥.
    Input Format: The input consists of a single integer 1 ≤ 𝑛 ≤ 10^6.
    Output Format: In the first line, output the minimum number 𝑘 of operations needed to get 𝑛 from 1. In the second line output a sequence of intermediate numbers. That is, the second line would contain positive integers 𝑎0, 𝑎2, . . . , 𝑎𝑘−1 such that 𝑎0 = 1, 𝑎𝑘−1 = 𝑛 and for all 0 ≤ 𝑖 < 𝑘 − 1, 𝑎𝑖+1 is equal to either 𝑎𝑖 + 1, 2𝑎𝑖, or 3𝑎𝑖.

---
+++ PrimitiveCalculator.py
@@ -0,0 +1,68 @@
+"""
+Primitive Calculator (Dynamic Programming)
+==================================
+
+@author: mhtehrani
+September 07, 2021
+https://github.com/mhtehrani
+
+"""
+
+def Optimal_Sequence(n):
+    Value = [None]*(n+1)
+    Sequence = [None]*(n+1)
+    
+    int_Val = [0,0,1,1]
+    ini_Seq = [[0],[1],[1,2],[1,3]]
+    
+    if n <= 3:
+        return int_Val[n], ini_Seq[n]
+    else:
+        Value[0:4] = [0,1,1,1]
+        for i in range(4,n+1):
+            Value[i] = Value[i-1]+1
+            if i % 3 == 0:
+                if Value[int(i/3)]+1 < Value[i]:
+                    Value[i] = Value[int(i/3)]+1
+            #end
+            if i % 2 == 0:
+                if Value[int(i/2)]+1 < Value[i]:
+                    Value[i] = Value[int(i/2)]+1
+            #end
+        #end
+    #end
+    
+    Sequence = [None]*(Value[n]+1)
+    Sequence[-1] = n
+    Sequence[0] = 1
+    
+    for j in range(Value[n]-1,0,-1):
+        n_tem = Sequence[j+1]
+        Sequence[j] = Sequence[j+1]-1
+        # if n_tem % 3 == 0:
+        #     if Value[int(n_tem/3)] == Value[Sequence[j+1]]-1:
+        #         Sequence[j] = int(n_tem/3)
+        #     #end
+        # #end
+        
+        if n_tem % 2 == 0:
+            if Value[int(n_tem/2)] == Value[Sequence[j+1]]-1:
+                Sequence[j] = int(n_tem/2)
+            #end
+        #end
+        
+        if n_tem % 3 == 0:
+            if Value[int(n_tem/3)] == Value[Sequence[j+1]]-1:
+                Sequence[j] = int(n_tem/3)
+            #end
+        #end
+    return Value[n], Sequence
+
+
+
+
+n = int(input())
+
+O_Value, O_Sequence = Optimal_Sequence(n)
+print(O_Value)
+print(*O_Sequence, sep=' ')
