23d1f8c0f1e472fabb6845bb07fff5b48a85e731
Mohammad H. Tehrani <tehran_moonlight@yahoo.com>
2021-09-26 19:52:00 -0700

    Add files via upload
    Edit Distance
    =============
    The edit distance between two strings is the minimum number of operations (insertions, deletions, and substitutions of symbols) to transform one string into another. It is a measure of similarity of two strings. The goal is to implement the algorithm for computing the edit distance between two strings.
    Input Format: Each of the two lines of the input contains a string consisting of lower case latin letters.
    Constraints: The length of both strings is at least 1 and at most 100.
    Output Format: Output the edit distance between the given two strings.

---
+++ EditDistance_DynamicProgramming.py
@@ -0,0 +1,41 @@
+"""
+Edit Distance (Dynamic Programming)
+===================================
+
+@author: mhtehrani
+September 07, 2021
+https://github.com/mhtehrani
+
+"""
+
+import numpy
+
+def Edit_Distance(row, column):
+    Dis = numpy.zeros([len(row)+1, len(column)+1])
+    
+    for i in range(1,len(row)+1):
+        Dis[i,0] = Dis[i-1,0]+1
+    #end
+    
+    for j in range(1,len(column)+1):
+        Dis[0,j] = Dis[0,j-1]+1
+    #end
+    
+    for i in range(1,len(row)+1):
+        for j in range(1,len(column)+1):
+            if row[i-1] == column[j-1]:
+                val = 0
+            else:
+                val = 1;
+            #end
+            
+            Dis[i,j] = min(Dis[i,j-1]+1,Dis[i-1,j]+1,Dis[i-1,j-1]+val)
+        #end
+    #end
+    
+    return int(Dis[len(row),len(column)])
+
+row = input()
+column = input()
+
+print(Edit_Distance(row, column))
