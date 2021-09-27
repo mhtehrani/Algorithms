d96b25ea6111f74522b0b94d63211f73183579c7
Mohammad H. Tehrani <tehran_moonlight@yahoo.com>
2021-09-26 19:00:52 -0700

    Update 3-wayQuickSort.py
    3-Way Quick Sort Algorithm
    ==========================
    The goal is to replace a 2-way partition with a 3-way partition in quick sort algorithm. That is, the new partition procedure should partition the array into three parts: < 𝑥 part, = 𝑥 part, and > 𝑥 part.
    Input Format: The first line of the input contains an integer 𝑛. The next line contains a sequence of 𝑛 integers 𝑎0, 𝑎1, . . . , 𝑎𝑛−1.
    Constraints: 1 ≤ 𝑛 ≤ 10^5; 1 ≤ 𝑎𝑖 ≤ 10^9 for all 0 ≤ 𝑖 < 𝑛.
    Output Format: Output this sequence sorted in non-decreasing order.

--- 3-wayQuickSort.py
+++ 3-wayQuickSort.py
@@ -10,16 +10,6 @@ https://github.com/mhtehrani
 
 import random
 
-def partition2(a, l, r):
-    x = a[l]
-    j = l
-    for i in range(l + 1, r + 1):
-        if a[i] <= x:
-            j += 1
-            a[i], a[j] = a[j], a[i]
-    a[l], a[j] = a[j], a[l]
-    return j
-
 def partition3(a, l, r):
     x = a[l]
     left = l
@@ -47,42 +37,9 @@ def randomized_quick_sort3(a, l, r):
     randomized_quick_sort(a, l, m - 1);
     randomized_quick_sort(a, n + 1, r);
 
-def randomized_quick_sort(a, l, r):
-    if l >= r:
-        return
-    k = random.randint(l, r)
-    a[l], a[k] = a[k], a[l]
-    #use partition3
-    m = partition2(a, l, r)
-    randomized_quick_sort(a, l, m - 1);
-    randomized_quick_sort(a, m + 1, r);
-
-
 
 n = int(input())
 a = list(map(int, input().split()))
 
-# n = 19
-# a = [9,9,9,9,1,1,1,1,2,2,2,2]
 randomized_quick_sort3(a, 0, n - 1)
 print(' '.join(map(str, a)))
-
-# randomized_quick_sort(a, 0, n - 1)
-# print(' '.join(map(str, a)))
-
-# while True:
-#     a = []
-#     n = random.randint(1, 100)
-#     for i in range(n):
-#         a.append(random.randint(10000000, 1000000000))
-    
-#     b = a[:]
-#     randomized_quick_sort3(a, 0, n - 1)
-#     b.sort()
-    
-#     if b != a:
-#         print(' '.join(map(str, a)))
-#         print(' '.join(map(str, b)))
-#         break
-    
-#     print("OK")
